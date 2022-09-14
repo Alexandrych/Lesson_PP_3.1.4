a7a2919e3cfa282ab1872b43dd24e3ae0db0903c
Alexandrych <alexandrych@mail.ru>
2022-09-15 00:03:12 +0300

    V1

--- src/main/java/com/example/lesson_pp_314/configs/AdminConfig.java
+++ src/main/java/com/example/lesson_pp_314/configs/AdminConfig.java
@@ -5,6 +5,7 @@ import com.example.lesson_pp_314.dao.UserDao;
 import com.example.lesson_pp_314.model.User;
 import com.example.lesson_pp_314.model.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,21 +16,20 @@ public class AdminConfig {
     private final RoleDao roleDao;
     private final PasswordEncoder passwordEncoder;
 
-    @Autowired
     public AdminConfig(UserDao userDao, RoleDao roleDao) {
         this.userDao = userDao;
         this.roleDao = roleDao;
         this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
-    @Autowired
+    @Bean
     protected void addAdminIfNotExist() {
         if (userDao.findByUsername("admin@admin.org") == null) {
             createAdmin();
         }
     }
 
-    protected void createAdmin() {
+    private void createAdmin() {
         User user = new User();
         user.setUsername("admin@admin.org");
         user.setFirstName("Stanislav");
